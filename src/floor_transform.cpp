64998b88ed5d31e025345bda987d8104f3e7abd5
Sebastian Pütz <spuetz@uos.de>
2014-05-15 17:08:45 +0200

    folder structure

---
+++ src/floor_transform.cpp
@@ -0,0 +1,31 @@
+/*
+ * UOS-ROS packages - Robot Operating System code by the University of Osnabrück
+ * Copyright (C) 2013 University of Osnabrück
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ * floor_transform.cpp
+ *
+ *  Created on: 15.05.2013
+ *     Authors: Sebastian Puetz <spuetz@uni-osnabrueck.de>
+ */
+
+#include <slam6d_node.h>
+
+namespace floor_transform{
+
+
+
+}
